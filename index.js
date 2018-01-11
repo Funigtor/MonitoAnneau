0d748184c676ff2014c584e0f59cc59c3b81bfb9
Steamyfuury <henri.hoyez@gmail.com>
2018-01-11 17:25:21 +0100

    ajout de la suppression.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http'); //require serveur WEB
 var url = require('url'); //pour les URL
 var querystring = require('querystring');
 var MongoClient = require("mongodb").MongoClient;
+var MongoObjectID = require("mongodb").ObjectID;
 const fs = require('fs');
 fs.writeFileSync("/tmp/pidMonito", process.pid);
 
@@ -13,61 +14,86 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     "Content-Type": "text/plain"
   });
-    console.log("[+] connection to Monitoanneau.")
+  console.log("[+] connection to Monitoanneau.")
   MongoClient.connect("mongodb://monito:friteusse@145.239.78.38:587/monitoanneau", function(error, client) {
-      if (error) throw error;
-      console.log("[+] connected to Monitoanneau/");
-      var db = client.db('monitoanneau');
-      selectOption();
-      return;
-      function selectOption() {
-        if ('insert' in params) {
-          insertInBDD();
-        }
-        if ('find' in params) {
-          findInBDD();
-        }
-      }
-      function findInBDD() {
-        if ('collection' in params) {
-          db.collection(params['collection']).find().toArray(function(error, results) {
-            if (error) throw error;
-            if (results.length != 0) {
-              devices = results;
-              selectInbDD();
-            } else {
-              console.error("[!] collection " + params['collection'] + " not found.");
-            }
-          });
-        } else
-          console.error("[!] pas de nom de coollection...");
+    if (error) throw error;
+    console.log("[+] connected to Monitoanneau/");
+    var db = client.db('monitoanneau');
+    selectOption();
+    return;
 
+    function selectOption() {
+      if ('insert' in params) {
+        insertInBDD();
       }
-      function selectInbDD() {
-        var tmp = params;
-        delete tmp.find;
-        delete tmp.collection;
-        for (key in params) {
-          var tab = new Array();
-          devices.forEach(function(elmnt, index) {
-            if (elmnt[key] === params[key]) tab.push(elmnt);
-          });
-          devices = tab;
-        }
-        res.write(JSON.stringify(devices));
-        res.end();
+      if ('find' in params) {
+        findInBDD();
       }
+      if ('erase' in params) {
+        eraseInBDD();
+      }
+    }
 
-      function insertInBDD(newObj) {
-        var tmp = params;
-        delete tmp.insert;
-        db.collection(params['collection']).insert(params, null, function(error, results) {
+    function findInBDD() {
+      if ('collection' in params) {
+        db.collection(params['collection']).find().toArray(function(error, results) {
           if (error) throw error;
-          console.log("[+] Document inséré")
+          if (results.length != 0) {
+            devices = results;
+            selectInbDD();
+          } else {
+            console.error("[!] collection " + params['collection'] + " empty");
+          }
         });
-        res.end();
-        return
+      } else
+        console.error("[!] pas de nom de coollection...");
+
+    }
+
+    function selectInbDD() {
+      var tmp = params;
+      delete tmp.find;
+      delete tmp.collection;
+      for (key in params) {
+        var tab = new Array();
+        devices.forEach(function(elmnt, index) {
+          if (elmnt[key] === params[key]) tab.push(elmnt);
+        });
+        devices = tab;
       }
+      res.write(JSON.stringify(devices));
+      res.end();
+    }
+
+    function insertInBDD(newObj) {
+      var tmp = params;
+      delete tmp.insert;
+      db.collection(params['collection']).insert(params, null, function(error, results) {
+        if (error) throw error;
+        console.log("[+] Document inséré")
+      });
+      res.end();
+      return
+    }
+
+    function eraseInBDD() {
+
+      if ('collection' in params){
+        if ('id' in params) {
+          db.collection(params['collection']).deleteOne({
+            _id: new require("mongodb").ObjectID(params['id'])
+          }, function(err, results) {
+            if (err) {
+              console.log("[!] failed");
+              throw err;
+            }
+            console.log("[+] success");
+          });
+          res.end()
+        }
+      }else console.log("[!] collection not found");
+    }
+    res.end();
   });
 });
 server.listen(8080);
