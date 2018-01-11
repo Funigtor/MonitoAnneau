49c40d8b550f11893144f8dc4383c432317840fb
Steamyfuury <henri.hoyez@gmail.com>
2018-01-11 15:03:50 +0100

    PUTAIN J'AI RÉUSSI LES GENS C'EST TROP BIEN POUCE BLEU HANDSPINNER MON CHAT DANS LA CUISINE AVEC NACIM EST GABRIEL ET BIENSÛR NICOLAS SARCOZY

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var url = require('url'); //pour les URL
 var querystring = require('querystring');
 var MongoClient = require("mongodb").MongoClient;
 const fs = require('fs');
-fs.writeFile("/tmp/pidMonito",process.pid);
+fs.writeFileSync("/tmp/pidMonito", process.pid);
 
 var server = http.createServer(function(req, res) {
 
@@ -15,121 +15,66 @@ var server = http.createServer(function(req, res) {
   });
 
   MongoClient.connect("mongodb://monito:friteusse@145.239.78.38:587/monitoanneau", function(error, client) {
-    if (error) throw error;
-
-    console.log("connected to Monitoanneau");
-    var db = client.db('monitoanneau');
-
-    for(key in params){console.log(key);}
-
-
-    if ('piece' in params) {
-      db.collection(params['piece']).find().toArray(function(error, results) {
-        if (error) throw error;
-        if (results.length != 0) {
-          devices = results;
-          console.log("coollections " + results.length);
-          desync(db);
-        } else {
-          console.error("collection " + params['piece'] + " not found.");
-          //res.write("collection " + params['device'] + " not found.");
-          //res.end();
+      if (error) throw error;
+      console.log("connected to Monitoanneau");
+      var db = client.db('monitoanneau');
+      selectOption();
+      return;
+      function selectOption() {
+        console.log("params" + JSON.stringify(params));
+        if ('insert' in params) {
+          console.log("insert");
+          insertInBDD();
         }
-      });
-    }
+        if ('find' in params) {
+          console.log("find");
+          findInBDD();
+        }
+      }
+      function findInBDD() {
+        if ('collection' in params) {
+          db.collection(params['collection']).find().toArray(function(error, results) {
+            if (error) throw error;
+            if (results.length != 0) {
+              devices = results;
+              console.log("coollections " + results.length);
+              selectInbDD();
+            } else {
+              console.error("collection " + params['piece'] + " not found.");
+            }
+          });
+        } else
+          console.error("pas de nom de coollection...");
+
+      }
+      function selectInbDD() {
+        var tmp = params;
+        delete tmp.find;
+        delete tmp.collection;
+        for (key in params) {
+          console.log(key);
+          var tab = new Array();
+          devices.forEach(function(elmnt, index) {
+            if (elmnt[key] === params[key]) tab.push(elmnt);
+          });
+          devices = tab;
+        }
+
+        res.write(JSON.stringify(devices));
+        res.end();
+      }
+
+      function insertInBDD(newObj) {
+        var tmp = params;
+        delete tmp.insert;
+        console.log(JSON.stringify(params));
+        db.collection(params['collection']).insert(params, null, function(error, results) {
+          if (error) throw error;
+          console.log("Document inséré")
+        });
+        res.end();
+        return
+      }
   });
-
-  function desync(db) {
-    console.log("[?] params :" + JSON.stringify(params));
-
-    //console.log(devices);
-    if ('input' in params) {
-      params.input = undefined;
-      db.collection(params.piece).insert(params, null, function(error, results) {
-        if (error) throw error;
-        console.log("Document inséré")
-      });
-      res.end();
-      return
-    }
-
-    if ('device' in params) {
-
-      var tmp = new Array();
-      devices.forEach(function(elmnt, index) {
-        if (elmnt.device === params['device']) tmp.push(elmnt);
-        //console.log(elmnt);
-      });
-      devices = tmp;
-      if (devices.length === 0) {
-        console.error(params['device'] + "n'existe pas ! \n exit()");
-        return;
-      }
-      //console.log(device);
-    }
-    var dateDebut = false;
-    var dateFin = false;
-    if ('dd' in params) {
-      dateDebut = isADate(params['dd']);
-      console.log(dateDebut);
-    }
-    if ('df' in params) {
-      dateFin = isADate(params['df']);
-      console.log(dateFin);
-    }
-    if (dateDebut || dateFin) {
-      tmp = new Array();
-      devices.forEach(function(obj, i) {
-        if (inInterval(dateDebut, dateFin, obj.date[0], obj.heure[0])) {
-          tmp.push(obj);
-          //console.log("[+] Push");
-        }
-      });
-      devices = tmp;
-    }
-    var string = JSON.stringify(devices)
-    res.write(string);
-    res.end();
-    console.log("[+] Done.");
-  }
-
-  function isADate(chaine) {
-    var regDate = new RegExp("\\d\\d/\\d\\d/\\d\\d\\d\\d");
-    if (regDate.test(chaine)) {
-      //console.log(chaine + " passe dans l'expression rég");
-      var regex = /\s?([/])\s?/;
-      var resultat = chaine.split(regex);
-      resultat = [parseInt(resultat[0]), parseInt(resultat[2]), parseInt(resultat[4])];
-      if (resultat[0] <= 31 && resultat[1] < 12) {
-        return resultat;
-      }
-    } else console.error(chaine + " n'est PAS une date !!!");
-  }
-
-  function inInterval(debut, fin, date, heure) {
-    if (!debut) {
-      if (parseInt(date.jour) <= fin[0] && parseInt(date.mois) <= fin[1] && parseInt(date.annee) <= fin[2]) {
-        //console.log(date.jour, fin[0] , parseInt(date.mois) , fin[1] , parseInt(date.annee) , fin[2]);
-        //console.log(parseInt(date.jour), debut[0] , parseInt(date.mois) , debut[1] , parseInt(date.annee) , debut[2]);
-        //console.log("true");
-        return true;
-      }
-    }
-    if (!fin) {
-      if (parseInt(date.jour) >= debut[0] && parseInt(date.mois) >= debut[1] && parseInt(date.annee) >= debut[2]) {
-        //console.log(date.jour, fin[0] , parseInt(date.mois) , fin[1] , parseInt(date.annee) , fin[2]);
-        //console.log(parseInt(date.jour), debut[0] , parseInt(date.mois) , debut[1] , parseInt(date.annee) , debut[2]);
-        //console.log("true");
-        return true;
-      }
-    }
-    if ((parseInt(date.jour) <= fin[0] && parseInt(date.mois) <= fin[1] && parseInt(date.annee) <= fin[2]) && (parseInt(date.jour) >= debut[0] && parseInt(date.mois) >= debut[1] && parseInt(date.annee) >= debut[2])) {
-      //console.log(date.jour, fin[0] , parseInt(date.mois) , fin[1] , parseInt(date.annee) , fin[2]);
-      //console.log(parseInt(date.jour), debut[0] , parseInt(date.mois) , debut[1] , parseInt(date.annee) , debut[2]);
-      console.log("true");
-      return true;
-    }
-    return false;
-  }
 });
 server.listen(8080);
