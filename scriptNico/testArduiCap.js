3830c2f85b5c80ac49754e69df85c307e07ea6a5
NicolasDESPRIET <nicolas.despriet@isen.yncrea.fr>
2018-01-11 15:55:58 +0100

    azertyuioi

---
+++ scriptNico/testArduiCap.js
@@ -0,0 +1,43 @@
+var MongoClient = require("mongodb").MongoClient;
+MongoClient.connect("mongodb://monito:friteusse@145.239.78.38:587/monitoanneau", function(error, client) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données");
+    var db = client.db('monitoanneau');
+
+    var d = 0;
+    var SerialPort = require('serialport');
+    const Readline = SerialPort.parsers.Readline;
+    const port = new SerialPort('/dev/ttyACM0');
+    const parser = new Readline();
+    port.pipe(parser);
+    parser.on('data', move);
+
+    function move(data ){
+      var start = new Date();
+      data = data.substring(0,data.length-1);
+      if (d == 1){
+        var chambre={
+          device: "presence",
+          date: [{jour: start.getDate(),mois: (start.getMonth()+1), annee: start.getFullYear()}],
+          heure: [{heure: start.getHours(), minute: start.getMinutes()}],
+          value: data};
+          d=0;
+      }else if (d == 0){
+        var chambre={
+          device: "temperature",
+          date: [{jour: start.getDate(),mois: (start.getMonth()+1), annee: start.getFullYear()}],
+          heure: [{heure: start.getHours(), minute: start.getMinutes()}],
+          value: data};
+        d=1;
+      }
+
+      //console.log(start.getHours()+':'+start.getMinutes()+':'+start.getSeconds()+'---'+start.getDate()+':'+(start.getMonth()+1)+':'+start.getFullYear());
+      //console.log(data);
+      db.collection("chambre").insert(chambre,null,function(error,results){
+        if (error) throw error;
+        console.log("donnees envoye");
+      })
+    }
+
+});
